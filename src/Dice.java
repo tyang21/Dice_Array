56aea7df4a745d1a13823bbac45dbac9aff69eac
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-10-29 00:59:05 -0400

    Add files via upload

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,43 @@
 
-public class Dice {
-
+public class Dice 
+{
+	//fields
+		private int numRoll; 
+		
+		//constructors
+		/**
+		 * constructing a Dice object
+		 */
+			public Dice()
+			{
+				numRoll = 0;		
+			}
+			
+		//methods
+		/**
+		 * Reseting the dice number of rolls to 0
+		 */
+		public void reset()
+		{
+				numRoll = 0;
+		}
+		
+		/**
+		 * getting roll number
+		 * @return numRoll
+		 */
+		public int numRoll()
+			{
+				return numRoll;
+			}
+		/**
+		 * rolling the Dice
+		 * @return integer value dice returns (1-6)
+		 */
+		public int Roll()
+			{
+				numRoll++;
+				return (int)(Math.random()*6) + 1;
+			
+			}
 }
--- src/DiceArrray.java
+++ src/DiceArrray.java
@@ -2,8 +2,19 @@
 public class DiceArrray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Dice dice1 = new Dice();
+		Dice dice2 = new Dice();
+		int[] count = new int[13];
+		
+		for (int i = 0; i<1000;i++)
+		{
+			int diceSum = dice1.Roll() + dice2.Roll();
+			count[diceSum]++;
+		}
+		for (int i = 2; i<13; i++)
+		{
+			System.out.println("Rolls of " + i + " : " + count[i]);
+		}
 	}
 
 }
